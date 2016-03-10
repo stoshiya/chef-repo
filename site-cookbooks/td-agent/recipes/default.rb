b211c893a3fd9ef622893f51a9b794a2a554793b
Toshiya SAITOH <stoshiya@gmail.com>
2016-03-10 18:49:22 +0900

    Added yum install packages and gem update.

--- site-cookbooks/td-agent/recipes/default.rb
+++ site-cookbooks/td-agent/recipes/default.rb
@@ -7,6 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+[ "gcc", "gcc-c++", "automake", "autoconf", "libtool", "zlib-devel", "ruby-devel" ].each do |p|
+  package p do
+    action :install
+  end
+end
+
 remote_file "/tmp/install-redhat.sh" do
   source "http://toolbelt.treasuredata.com/sh/install-redhat.sh"
 end
@@ -17,6 +23,15 @@ script "install" do
   code <<-EOL
     sed -i -e 's/^sudo -k/#sudo -k/' -e 's/^sudo sh/sh/' /tmp/install-redhat.sh
     sh /tmp/install-redhat.sh
+  EOL
+end
+
+script "plugin update and install" do
+  interpreter "bash"
+  user "root"
+  code <<-EOL
+    sudo /usr/lib64/fluent/ruby/bin/fluent-gem update --no-ri --no-rdoc
+    sudo /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-forest --no-ri --no-rdoc
   EOL
 end
 
