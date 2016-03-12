d40ebd45d4164d930b84180fffed6af8b035cde1
Toshiya SAITOH <stoshiya@gmail.com>
2016-03-12 10:34:56 +0900

    Simplified installation.

--- site-cookbooks/td-agent/recipes/default.rb
+++ site-cookbooks/td-agent/recipes/default.rb
@@ -17,26 +17,15 @@ remote_file "/tmp/install-redhat-td-agent2.sh" do
   source "https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh"
 end
 
-script "install" do
-  interpreter "bash"
-  user "root"
-  code <<-EOL
-    sed -i -e 's/^sudo -k/#sudo -k/' -e 's/^sudo sh/sh/' /tmp/install-redhat-td-agent2.sh
-    sh /tmp/install-redhat-td-agent2.sh
-  EOL
-end
-
-script "install plugin" do
-  interpreter "bash"
-  user "root"
-  code <<-EOL
-    /opt/td-agent/usr/sbin/td-agent-gem install fluent-plugin-forest --no-ri --no-rdoc
-  EOL
+execute "install" do
+  command "sh /tmp/install-redhat-td-agent2.sh"
 end
 
 directory "/etc/td-agent/conf.d" do
   owner "root"
-  mode  0755
+  group "root"
+  mode 0755
+  recursive true
 end
 
 template "td-agent.conf" do
