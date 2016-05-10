972afec17995efa4bdf1be02fb04df3177abf577
Toshiya SAITOH <stoshiya@gmail.com>
2016-05-10 19:21:34 +0900

    Enabled to output to elasticsearch.

--- site-cookbooks/td-agent/attributes/default.rb
+++ site-cookbooks/td-agent/attributes/default.rb
@@ -1,1 +1,7 @@
-default['td-agent']['config'] = []
+default['td-agent']['logs'] = []
+default['td-agent']['s3'] = false
+default['td-agent']['s3_bucket'] = 'bucket name'
+default['td-agent']['s3_region'] = 'ap-northeast-1'
+default['td-agent']['elasticsearch'] = false
+default['td-agent']['elasticsearch_host'] = 'localhost'
+default['td-agent']['elasticsearch_port'] = 9200
--- site-cookbooks/td-agent/recipes/default.rb
+++ site-cookbooks/td-agent/recipes/default.rb
@@ -17,8 +17,13 @@ remote_file "/tmp/install-redhat-td-agent2.sh" do
   source "https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh"
 end
 
-execute "install" do
-  command "sh /tmp/install-redhat-td-agent2.sh"
+script "install" do
+  interpreter "bash"
+  user "root"
+  code <<-EOL
+    sh /tmp/install-redhat-td-agent2.sh
+    /usr/sbin/td-agent-gem install fluent-plugin-elasticsearch
+  EOL
 end
 
 directory "/etc/td-agent/conf.d" do
@@ -36,20 +41,24 @@ template "td-agent.conf" do
   mode 0644
 end
 
-node['td-agent']['config'].each do |config|
-  template "#{config.dir}-#{config.name}.conf" do
-    path "/etc/td-agent/conf.d/#{config.dir}-#{config.name}.conf"
-    source "template.s3.conf.erb"
+node['td-agent']['logs'].each do |log|
+  template "#{log.dir}-#{log.name}.conf" do
+    path "/etc/td-agent/conf.d/#{log.dir}-#{log.name}.conf"
+    source "template.tail-in.conf.erb"
     owner "root"
     group "root"
     mode 0644
     variables(
-      :name        => config.name,
-      :dir         => config.dir,
-      :format      => config.format,
-      :time_format => config.time_format,
-      :s3_bucket => node['td-agent']['s3_bucket'],
-      :s3_region => node['td-agent']['s3_region']
+      :name        => log.name,
+      :dir         => log.dir,
+      :format      => log.format,
+      :time_format => log.time_format,
+      :s3                 => node['td-agent']['s3'],
+      :s3_bucket          => node['td-agent']['s3_bucket'],
+      :s3_region          => node['td-agent']['s3_region'],
+      :elasticsearch      => node['td-agent']['elasticsearch'],
+      :elasticsearch_host => node['td-agent']['elasticsearch_host'],
+      :elasticsearch_port => node['td-agent']['elasticsearch_port']
     )
   end
 end
