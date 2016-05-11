cbe0dd52a8a99c7923fb4af5842041980e624d7f
Toshiya SAITOH <stoshiya@gmail.com>
2016-05-11 17:22:36 +0900

    Removed package installing.

--- site-cookbooks/td-agent/recipes/default.rb
+++ site-cookbooks/td-agent/recipes/default.rb
@@ -7,12 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-[ "gcc", "gcc-c++", "automake", "autoconf", "libtool", "zlib-devel", "ruby-devel" ].each do |p|
-  package p do
-    action :install
-  end
-end
-
 remote_file "/tmp/install-redhat-td-agent2.sh" do
   source "https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh"
 end
